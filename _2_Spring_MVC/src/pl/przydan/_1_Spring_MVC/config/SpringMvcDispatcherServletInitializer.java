551e13ca7c96580b8cd39dab684939bc2e7c0ca6
Przydan <patryk@przydanek.com>
2020-01-22 12:36:31 +0100

    #Spring course — Spring WebApp configuration — added JSP homePage — Practise with Spring MVC

---
+++ _2_Spring_MVC/src/pl/przydan/_1_Spring_MVC/config/SpringMvcDispatcherServletInitializer.java
@@ -0,0 +1,20 @@
+//package pl.przydan._1_Spring_MVC.config;
+//
+//import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+//
+//public class SpringMvcDispatcherServletInitializer  extends AbstractAnnotationConfigDispatcherServletInitializer {
+//    @Override
+//    protected Class<?>[] getRootConfigClasses() {
+//        return new Class[0];
+//    }
+//
+//    @Override
+//    protected Class<?>[] getServletConfigClasses() {
+//        return new Class[0];
+//    }
+//
+//    @Override
+//    protected String[] getServletMappings() {
+//        return new String[0];
+//    }
+//}
