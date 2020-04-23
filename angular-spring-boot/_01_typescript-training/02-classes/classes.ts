bde6e44dee01a0df398877cd0f923dbcb27ed9a0
Przydan <przydan@hotmail.com>
2020-04-23 12:43:19 +0200

    - constructor short form

--- angular-spring-boot/_01_typescript-training/02-classes/classes.ts
+++ angular-spring-boot/_01_typescript-training/02-classes/classes.ts
@@ -32,16 +32,30 @@ class Customer2 {
     }
 }
 
+class Customer3 {
+    constructor(private _firstName: string, private _lastName: string) { }
+
+    public get firstName(): string {
+        return this._firstName;
+    }
+
+    public get lastName(): string {
+        return this._lastName;
+    }
+}
+
 let myCustomer = new Customer();
 
 myCustomer._firstName = "Mario";
 myCustomer._lastName = "Calzone";
-
 console.log(`customer1: ${myCustomer._firstName} ${myCustomer._lastName}`);
 
 let myCustomer2 = new Customer2("Luigi", "Bergamo");
-
 console.log("Customer2: " + myCustomer2.firstName + " " + myCustomer2.lastName);
+
+let myCustomer3 = new Customer3("Aurora", "Borealis");
+console.log(`customer3: ${myCustomer3.firstName} ${myCustomer3.lastName} ` )
+
 
 console.log("##############################################");
 console.log("end");
